4fbc65c9147712d465aa0433eb0c7bb3a307f9f1
RDYS0Raiden <luis0ticona2@gmail.com>
2022-09-18 12:17:36 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoRs/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoRs.Models;
+using System.Diagnostics;
+using ProyectoRs.Models;
+using ProyectoRs.Servicios;
+namespace ProyectoRs.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        {
+            _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+        }
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return View("Gracias");
+        }
+        public IActionResult Gracias()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoRs/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,45 @@
+﻿using ProyectoRs.Models;
+
+namespace ProyectoRs.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+            {
+                Titulo="CHURASQUERIA1",
+                Descripcion="pagina para buscar una churasqueria",
+                Link="https://www.boliviaentusmanos.com/restaurantes-gastronomia/1/churrasquerias.html",
+                ImagenURL="/imagenes/brangus1.png"
+            },
+                 new Proyecto
+            {
+                Titulo="CHURASQUERIA2",
+                Descripcion="churrasqueria en todo bolivia",
+                Link="https://amarillas.bo/rubro/churrasquerias",
+                ImagenURL="/imagenes/brangus2.png"
+            },
+                  new Proyecto
+            {
+                Titulo="Proccimo Proyecto",
+                Descripcion="Proyecto en proceso",
+                Link="",
+                ImagenURL="/imagenes/brangus3.png"
+            },
+                new Proyecto
+            {
+                Titulo="Proccimo Proyecto",
+                Descripcion="proyecto en proceso",
+                Link="https://www.google.com",
+                ImagenURL="/imagenes/brangus1.png"
+            },
+            };
+        }
+    }
+}
